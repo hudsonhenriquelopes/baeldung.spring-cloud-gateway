ac6bd80a333b3f9a9ccba2fa2365d49879bbddd9
Hudson Henrique Lopes <hudsonhenriquelopes@gmail.com>
2026-05-25 21:01:23 -0300

    Fix issues

--- src/main/java/com/hch/SpringCloudGatewayApplication.java
+++ src/main/java/com/hch/SpringCloudGatewayApplication.java
@@ -2,11 +2,12 @@ package com.hch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/* @EnableDiscoveryClient is not need anymore.
+ * Spring Cloud already discovers that and
+ * register on Eureka. */
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
 public class SpringCloudGatewayApplication {
 
